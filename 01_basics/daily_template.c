8639c34f11e7f9787e25fee85ad6cfd37b2cc514
Amanat Ullah <amanatkhattak533@gmail.com>
2026-04-02 12:05:22 +0500

    chore: add daily practice template

---
+++ 01_basics/daily_template.c
@@ -0,0 +1,34 @@
+/**
+ * Date: 2026-04-02
+ * Topic: Fundamentals
+ * Source: WsCube Tech C Course - Video Section 
+ * 
+ * What I learned today:
+ * 1. Properties Of C:
+     -Middle level Language(Both HLL & LLL).
+     -Efficient.
+     -Portable.
+     -Felxible.
+ * 2. Rules for writing C program
+     -Function Based.
+     -At least one function.
+     -Requires main().
+     -Start and end with main().
+     -Function inside function-Not allowed.
+     -Function can call another function.
+     -Declaration of function.
+     -Two types of function(pre and user defined).
+     -Case sensitive.
+     -Requires compiler.
+ * 
+ * Concepts I need to review:
+ * - 
+ */
+
+#include <stdio.h>
+
+int main() {
+    
+    
+    return 0;
+}
